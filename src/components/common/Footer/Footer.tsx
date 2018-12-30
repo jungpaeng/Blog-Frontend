539dcddb62ca15cb1882add52846325c8d36bbde
Yongbeen Im <meis1541@naver.com>
2018-12-30 18:02:24 +0900

    Footer 컴포넌트 작성 및 노출

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -1,13 +1,20 @@
 import * as React from 'react';
-import classNames from 'classnames';
+import classNames from 'classnames/bind';
+import {Link} from 'react-router-dom';
 import styles from './Footer.module.scss';
 
 const cx = classNames.bind(styles);
 
 const Footer: React.FunctionComponent = () => (
-    <div className={cx('footer')}>
-        Footer
-    </div>
+    <footer className={cx('footer')}>
+        <Link
+            to="/"
+            className={cx('brand')}
+        >
+            Blog
+        </Link>
+        <div className={cx('admin-login')}>로그인</div>
+    </footer>
 );
 
 export default Footer;
--- src/components/common/PageTemplate/PageTemplate.tsx
+++ src/components/common/PageTemplate/PageTemplate.tsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 import classNames from 'classnames/bind';
 import styles from './PageTemplate.module.scss';
 import Header from '../Header';
+import Footer from '../Footer';
 
 const cx = classNames.bind(styles);
 
 const PageTemplate: React.FunctionComponent = () => (
     <div className={cx('page-template')}>
         <Header />
+        <Footer />
     </div>
 );
 
