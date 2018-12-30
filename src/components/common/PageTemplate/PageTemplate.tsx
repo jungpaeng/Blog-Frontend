60c26d37b14591ffb3ea023227287e37719d72be
Yongbeen Im <meis1541@naver.com>
2018-12-30 17:33:40 +0900

    Header 컴포넌트 호출

--- src/components/common/PageTemplate/PageTemplate.tsx
+++ src/components/common/PageTemplate/PageTemplate.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import classNames from 'classnames/bind';
 import styles from './PageTemplate.module.scss';
+import Header from '../Header';
 
 const cx = classNames.bind(styles);
 
 const PageTemplate: React.FunctionComponent = () => (
     <div className={cx('page-template')}>
-        PageTemplate
+        <Header />
     </div>
 );
 
