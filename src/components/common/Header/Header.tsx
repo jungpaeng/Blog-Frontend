63475a892c9926e3ccfb1e24ae97e75e1df639bb
Yongbeen Im <meis1541@naver.com>
2018-12-30 17:53:23 +0900

    Header 컴포넌트 작성 및 스타일링

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,13 +1,21 @@
 import * as React from 'react';
-import classNames from 'classnames';
+import classNames from 'classnames/bind';
+import {Link} from 'react-router-dom';
 import styles from './Header.module.scss';
 
 const cx = classNames.bind(styles);
 
 const Header: React.FunctionComponent = () => (
-    <div className={cx('header')}>
-        Header
-    </div>
+    <header className={cx('header')}>
+        <div className={cx('header-content')}>
+            <div className={cx('brand')}>
+                <Link to="/">Blog</Link>
+            </div>
+            <div className={cx('right')}>
+                우측 컴포넌트
+            </div>
+        </div>
+    </header>
 );
 
 export default Header;
