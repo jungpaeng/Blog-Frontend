0141a3f7b0099c9a75f7fbd203bc36e6a000db3e
Yongbeen Im <meis1541@naver.com>
2018-12-30 14:07:24 +0900

    configure 생성
    - 스토어를 생성하는 함수
    - 서버사이드 렌더링을 할 때 서버에서 역시 호출해야 해 함수를 따로 작성

---
+++ src/store/configure.ts
@@ -0,0 +1,28 @@
+declare var window: Window & {__REDUX_DEVTOOLS_EXTENSION_COMPOSE__: any};
+
+import {
+    createStore,
+    applyMiddleware,
+    compose,
+    combineReducers,
+    AnyAction
+} from 'redux';
+import reduxPender from 'redux-pender';
+import * as modules from './modules';
+
+const reducers = combineReducers(modules);
+const middlewares = [reduxPender()];
+
+// Development mode
+const isDev = process.env.NODE_ENV === 'development';
+const devtools = isDev && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const composeEnhancers = devtools || compose;
+
+// preloadedState is ServerSideRendering initalState
+const configure = (preloadedState: AnyAction) => createStore(
+    reducers,
+    preloadedState,
+    composeEnhancers(applyMiddleware(...middlewares))
+);
+
+export default configure;
