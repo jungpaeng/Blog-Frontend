dbe7c0108c03c39dedab5011a0bf6131a8a08f9a
Yongbeen Im <meis1541@naver.com>
2018-12-30 14:19:48 +0900

    라우트 지정

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,18 @@
 import * as React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import {EditorPage, ListPage, NotFoundPage, PostPage} from '../pages';
 
 const App = () => {
     return (
         <div className="dodo">
-            Blog
+            <Switch>
+                <Route exact path="/" component={ListPage} />
+                <Route path="/page/:page" component={ListPage} />
+                <Route path="/tag/:tag/:page?" component={ListPage} />
+                <Route path="/post/:id" component={PostPage} />
+                <Route path="/editor" component={EditorPage} />
+                <Route component={NotFoundPage} />
+            </Switch>
         </div>
     );
 };
---
+++ src/pages/PostPage.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const PostPage = () => {
+    return (
+        <div>
+            Post
+        </div>
+    );
+};
+
+export default PostPage;
