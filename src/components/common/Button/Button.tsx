1bcfdf06fd59d26a19f1cbd62e78b88f8426a7e7
Yongbeen Im <meis1541@naver.com>
2018-12-30 18:33:46 +0900

    Button 컴포넌트 버그 수정
    - 인터페이스 타입 정의가 잘못되어 있음
    - onClick props를 갖고있을 때만 넘겨주도록 설정

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -6,10 +6,10 @@ import styles from './Button.module.scss';
 const cx = classNames.bind(styles);
 
 interface IButton {
-    to: string;
-    disabled: boolean;
-    theme: string;
-    onClick: (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void;
+    to?: string;
+    disabled?: boolean;
+    theme?: string;
+    onClick?: (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void;
 }
 
 const Div: React.FC = ({children, ...rest}) => (
@@ -23,17 +23,17 @@ const Button: React.FC<IButton> = ({
     disabled,
     theme = 'default'
 }) => {
-    const Element = (to && disabled) ? Link : Div;
+    const Element = (to && !disabled) ? Link : Div;
 
     return (
         <Element
-            to={to}
+            to={to ? to : ''}
             className={cx(
                 'button',
                 theme,
                 {disabled}
             )}
-            onClick={disabled ? () => null : onClick}
+            onClick={(onClick && !disabled) ? onClick : () => null}
         >
             {children}
         </Element>
