bf8a9ca68f77d3563997fbe1547a042f7360316c
Yongbeen Im <meis1541@naver.com>
2018-12-30 14:05:07 +0900

    리덕스 설정
    - base
    - editor
    - list
    - post

---
+++ src/store/modules/editor.ts
@@ -0,0 +1,15 @@
+import {createAction, handleActions} from 'redux-actions';
+import {pender} from 'redux-pender';
+import {Map} from 'immutable';
+
+// action tyles
+
+// action creators
+
+// initial state
+const initialState = Map({});
+
+// reducer
+export default handleActions({
+
+}, initialState);
---
+++ src/store/modules/index.ts
@@ -0,0 +1,5 @@
+export {penderReducer as pender} from 'redux-pender';
+export {default as base} from './base';
+export {default as editor} from './editor';
+export {default as list} from './list';
+export {default as post} from './post';
