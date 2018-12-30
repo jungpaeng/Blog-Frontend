2ef47c01f198c5f23ea73cfcd96886a64650601f
Yongbeen Im <meis1541@naver.com>
2018-12-31 00:25:20 +0900

    EditorPane 컴포넌트 내 마크다운 에디터 삽입

--- src/components/editor/EditorPane/EditorPane.tsx
+++ src/components/editor/EditorPane/EditorPane.tsx
@@ -1,10 +1,34 @@
 import * as React from 'react';
+import CodeMirror from 'codemirror';
+import 'codemirror/mode/markdown/markdown';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/jsx/jsx';
+import 'codemirror/mode/css/css';
+import 'codemirror/mode/shell/shell';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/monokai.css';
 import classNames from 'classnames/bind';
 import styles from './EditorPane.module.scss';
 
 const cx = classNames.bind(styles);
 
 class EditorPane extends React.Component {
+    editorRef: HTMLDivElement = {} as HTMLDivElement;
+    codeMirror: CodeMirror.Editor = {} as CodeMirror.Editor;
+
+    initializeEditor = () => {
+        this.codeMirror = CodeMirror(this.editorRef, {
+            lineNumbers: true,
+            lineWrapping: true,
+            mode: 'markdown',
+            theme: 'monokai'
+        });
+    }
+
+    componentDidMount() {
+        this.initializeEditor();
+    }
+
     render() {
         return (
             <div className={cx('editor-pane')}>
@@ -13,7 +37,10 @@ class EditorPane extends React.Component {
                     placeholder="제목을 입력하세요"
                     name="title"
                 />
-                <div className={cx('code-editor')} />
+                <div
+                    className={cx('code-editor')}
+                    ref={(ref: HTMLDivElement) => this.editorRef = ref}
+                />
                 <div className={cx('tags')}>
                     <div className={cx('description')}>태그</div>
                     <input
