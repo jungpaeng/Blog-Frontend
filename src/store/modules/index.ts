f11c4f95b82b34db2a0d6fa7da13bd75148278c5
Yongbeen Im <meis1541@naver.com>
2019-01-02 01:23:53 +0900

    post 리듀서 module 타입 정의

--- src/store/modules/index.ts
+++ src/store/modules/index.ts
@@ -1,16 +1,19 @@
 
 import {combineReducers} from 'redux';
-import {penderReducer as pender} from 'redux-pender';
+import {penderReducer as pender, PenderState} from 'redux-pender';
 // export {default as base} from './base';
 // export {default as list} from './list';
-// export {default as post} from './post';
+import post, {IPostState} from './post';
 import editor, {IEditorState} from './editor';
 
 export default combineReducers({
     editor,
+    post,
     pender
 } as any);
 
 export interface IStoreState {
     editor: IEditorState;
+    post: IPostState;
+    pender: PenderState;
 }
